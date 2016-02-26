a4c12398282132a2761c5321089756672319897b
Shivam Bharuka <shivam.bharuka@gmail.com>
2016-02-26 04:45:53 -0600

    tweetdetail

---
+++ Twitter-Demo/TweetDetailViewController.swift
@@ -0,0 +1,83 @@
+//
+//  TweetDetailViewController.swift
+//  Twitter-Demo
+//
+//  Created by Shivam Bharuka on 2/26/16.
+//  Copyright © 2016 Shivam Bharuka. All rights reserved.
+//
+
+import UIKit
+
+class TweetDetailViewController: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var username: UILabel!
+    @IBOutlet weak var screenname: UILabel!
+    @IBOutlet weak var tweettext: UILabel!
+    @IBOutlet weak var timestamp: UILabel!
+    @IBOutlet weak var retweet_count: UILabel!
+    @IBOutlet weak var favorites_count: UILabel!
+    @IBOutlet weak var retweet_image: UIButton!
+    @IBOutlet weak var favorite_image: UIButton!
+    
+    var tweet: Tweet!
+    var count_retweet: Int = 0
+    var count_favorite: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        profileImage.setImageWithURL((tweet.user?.profileUrl)!)
+        username.text = tweet.user?.name
+        screenname.text = "@\(tweet.user!.screenname!)"
+        tweettext.text = tweet.text
+        
+        count_favorite = tweet.favourites_count
+        count_retweet = tweet.retweet_count
+        retweet_count.text = "\(tweet.retweet_count)"
+        favorites_count.text = "\(tweet.favourites_count)"
+        
+        let dateformatter = NSDateFormatter()
+        dateformatter.dateStyle = NSDateFormatterStyle.ShortStyle
+        dateformatter.timeStyle = .ShortStyle
+        
+        let dateString = dateformatter.stringFromDate(tweet.timestamp!)
+        timestamp.text = dateString
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func retweetClicked(sender: AnyObject) {
+        TwitterClient.sharedInstance.retweet(["id": tweet.id!]) { (tweets, error) -> () in
+            if (tweets != nil){
+                self.retweet_count.text = "\(self.count_retweet+1)"
+                self.retweet_image.setImage(UIImage(named: "retweet-action-on-pressed"), forState: UIControlState.Normal)
+            }
+        }
+
+    }
+    
+    @IBAction func favoriteClicked(sender: AnyObject) {
+        TwitterClient.sharedInstance.favorite(["id": tweet.id!]) { (tweet, error) -> () in
+            
+            if (tweet != nil) {
+                self.favorite_image.setImage(UIImage(named: "like-action-on-pressed"), forState: UIControlState.Normal)
+                self.favorites_count.text = "\(self.count_favorite+1)"
+            }
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Twitter-Demo/TweetsViewController.swift
+++ Twitter-Demo/TweetsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class TweetsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class TweetsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UIScrollViewDelegate {
 
     var tweets: [Tweet]?
     
@@ -20,10 +20,16 @@ class TweetsViewController: UIViewController, UITableViewDataSource, UITableView
         tweetTable.delegate = self
         tweetTable.rowHeight = UITableViewAutomaticDimension
         tweetTable.estimatedRowHeight = 120
-            
+        
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "refreshControlAction:", forControlEvents: UIControlEvents.ValueChanged)
+        tweetTable.insertSubview(refreshControl, atIndex: 0)
+        
         TwitterClient.sharedInstance.homeTimeline(nil, completion: { (tweets, error) -> () in
-            self.tweets = tweets
-            self.tweetTable.reloadData()
+            if tweets != nil{
+                self.tweets = tweets
+                self.tweetTable.reloadData()
+            }
         })
         // Do any additional setup after loading the view.
     }
@@ -36,7 +42,15 @@ class TweetsViewController: UIViewController, UITableViewDataSource, UITableView
     @IBAction func onLogout(sender: AnyObject) {
         TwitterClient.sharedInstance.logout()
     }
-
+    
+    func refreshControlAction(refreshControl: UIRefreshControl) {
+        TwitterClient.sharedInstance.homeTimeline(nil, completion: { (tweets, error) -> () in
+            self.tweets = tweets
+            self.tweetTable.reloadData()
+            refreshControl.endRefreshing()
+        })
+    }
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if tweets != nil{
             return tweets!.count
@@ -52,15 +66,43 @@ class TweetsViewController: UIViewController, UITableViewDataSource, UITableView
         return cell
     }
     
+   var isMoreDataLoading = false
     
-    /*
+    func loadMoreData() {
+        TwitterClient.sharedInstance.homeTimeline(nil, completion: { (tweets, error) -> () in
+            self.tweets = tweets
+            self.isMoreDataLoading = false
+            self.tweetTable.reloadData()
+        })
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        if (!isMoreDataLoading) {
+            let scrollViewContentHeight = tweetTable.contentSize.height
+            let scrollOffsetThreshold = scrollViewContentHeight - tweetTable.bounds.size.height
+            
+            if(scrollView.contentOffset.y > scrollOffsetThreshold && tweetTable.dragging) {
+                print("Scrolling Called")
+                isMoreDataLoading = true
+                loadMoreData()
+            }
+            
+        }
+    }
+    
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
+        let cell = sender as! UITableViewCell
+        let indexPath = tweetTable.indexPathForCell(cell)
+        let tweet = tweets![indexPath!.row]
+        
+        let tweetdetailviewcontroller = segue.destinationViewController as! TweetDetailViewController
+        tweetdetailviewcontroller.tweet = tweet
     }
-    */
+    
 
 }
